e15db0a8c8d51f3f84cb2411ab789f51a488f143
PBL075 <pavelgorea75@gmail.com>
2026-05-02 10:43:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace whileeee
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+
+            do
+            {
+                System.Console.WriteLine(i);
+                i++;
+                
+            }while(i<=10);
+
+            
+        }
+    }
+}
